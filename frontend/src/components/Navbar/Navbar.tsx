238aedb9c04ec8576079aee277e77d3e53a21f42
Ingrid <ingrihkv@stud.ntnu.no>
2025-02-08 16:55:16 +0100

    feat: create burger menu and make responsive

--- frontend/src/components/Navbar/Navbar.tsx
+++ frontend/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,19 @@
 import { Link } from '@tanstack/react-router';
-import { CiUser } from 'react-icons/ci';
+import { FaUser } from "react-icons/fa6";
+import { LuMenu } from "react-icons/lu";
 
 function Navbar() {
   return (
     <nav className="bg-green-900 p-3">
-      <div className="flex items-center justify-between w-full">
-        <Link to="/" className="text-white text-5xl">PAM</Link>
-        <div className="flex items-center space-x-4">
-          <Link to="/" className="text-white text-2xl hover:underline">User</Link>
+      <div className="flex flex-wrap items-center justify-between w-full">
+        <div className="flex items-center space-x-6 ">
+          <Link to="/" className="text-white text-3xl sm:text-4xl">PAM</Link>
+          <LuMenu className="text-white text-3xl sm:text-4xl translate-y-0.5" />
+        </div>
+        <div className="flex items-center space-x-4 mt-2 sm:mt-0">
+          <Link to="/" className="hidden sm:block text-white text-lg sm:text-2xl">Username</Link>
           <Link to="/">
-            <CiUser className="text-white text-4xl" />
+            <FaUser className="text-white text-2xl sm:text-3xl" />
           </Link>
         </div>
       </div>
